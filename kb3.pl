f9367a70ff4501a987daf332703bb3fbcd30d1d7
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2021-05-07 09:18:31 -0300

    Adiciona Knowledge Bases do capítulo 1.1

---
+++ kb3.pl
@@ -0,0 +1,9 @@
+happy(vincent).
+listen2Music(butch).
+playsAirGuitar(vincent):-
+	listen2Music(vincent),
+	happy(vincent).
+playsAirGuitar(butch):-
+	happy(butch).
+playsAirGuitar(butch):-
+	listen2Music(butch).
